d488d76493ccaa8a70a37c7a0daee081713419d4
Victoria Esteves <138332052+VictoriaEsteves@users.noreply.github.com>
2023-11-13 20:18:43 -0300

    Rafa challenge

--- convertordeMoedas/desafios(3)/GuiLima/conversor.js
+++ convertordeMoedas/desafios(3)/GuiLima/conversor.js
@@ -1,6 +1,1 @@
 
-var valorBitCoin = 180894.60
-var bitCoin = 1
-var valorCalculado = bitCoin* valorBitCoin
-
-alert(valorCalculado)
---
+++ convertordeMoedas/desafios(3)/RafaBallerini/conversor.js
@@ -0,0 +1,6 @@
+
+var valorBitCoin = 180894.60
+var bitCoin = 1
+var valorCalculado = bitCoin* valorBitCoin
+
+alert(valorCalculado)
