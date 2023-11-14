357ae4c75ad3c1e9dcb97b681eadeccaf4309d64
Victoria Esteves <138332052+VictoriaEsteves@users.noreply.github.com>
2023-11-13 21:51:01 -0300

    Update conversor.js

--- convertordeMoedas/desafios(3)/RafaBallerini/conversor.js
+++ convertordeMoedas/desafios(3)/RafaBallerini/conversor.js
@@ -3,4 +3,5 @@ var nome = window.prompt('Qual é seu nome?')
 var valorConverter = prompt('Valor a converter')
 var cotacaoDollar = 4.91
 var valorEmReais = valorConverter * cotacaoDollar
+
 alert(`Olá ${nome}, a conversao do dolar para o real é ${valorEmReais}`)
