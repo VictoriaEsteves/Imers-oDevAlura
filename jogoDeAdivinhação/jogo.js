139a6decffb72906a6f68c31459ece61d188064b
Victoria Esteves <138332052+VictoriaEsteves@users.noreply.github.com>
2023-11-14 09:30:41 -0300

    Update jogo.js

---
+++ jogoDeAdivinhação/jogo.js
@@ -0,0 +1,10 @@
+
+var numSecreto = 58
+
+var chute = prompt(`Digite um numero de 1 a 1000`)
+
+if(chute == numSecreto) {
+    alert("Parabens voce acertou!!")
+} else {
+    alert("Anao... Voce errou...")
+}
