8e1ecf0f5a2eeb3960ad582c272869abbd2a9580
Victoria Esteves <138332052+VictoriaEsteves@users.noreply.github.com>
2023-11-13 21:49:12 -0300

    Coneversor com nome por prompt

--- convertordeMoedas/desafios(3)/RafaBallerini/conversor.js
+++ convertordeMoedas/desafios(3)/RafaBallerini/conversor.js
@@ -1,6 +1,6 @@
 
-var valorBitCoin = 180894.60
-var bitCoin = 1
-var valorCalculado = bitCoin* valorBitCoin
-
-alert(valorCalculado)
+var nome = window.prompt('Qual é seu nome?')
+var valorConverter = prompt('Valor a converter')
+var cotacaoDollar = 4.91
+var valorEmReais = valorConverter * cotacaoDollar
+alert(`Olá ${nome}, a conversao do dolar para o real é ${valorEmReais}`)
