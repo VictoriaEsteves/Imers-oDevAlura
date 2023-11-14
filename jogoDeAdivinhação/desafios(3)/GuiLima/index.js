29ba20dfe06040ddc36d6b3047b2cd4ee25c7276
Victoria Esteves <138332052+VictoriaEsteves@users.noreply.github.com>
2023-11-14 11:06:54 -0300

    Resolving bugs

---
+++ jogoDeAdivinhação/desafios(3)/GuiLima/index.js
@@ -0,0 +1,18 @@
+
+var numSecreto = parseInt(Math.random() * 1001 + 1)
+
+while(chute != numSecreto){
+
+    var chute = prompt("Digite um numero de 1 a 1000")
+
+    if(chute == numSecreto) {
+        alert("Parabens voce acertou!!")
+    } else if (chute < numSecreto) {
+        alert(`Você errou... o numero é maior que o numero digitado!`)
+    } else if (chute > numSecreto) {
+        alert(`Digite um numero valido!`)
+    } else {
+        alert(`Você errou... O numero é menor que o numero digitado!`)
+    }
+}
+
