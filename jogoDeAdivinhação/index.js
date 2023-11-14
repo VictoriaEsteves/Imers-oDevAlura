e3947706b57866f0608c02fcd599383db5a96734
Victoria Esteves <138332052+VictoriaEsteves@users.noreply.github.com>
2023-11-14 09:45:16 -0300

    Update index.js

--- jogoDeAdivinhação/index.js
+++ jogoDeAdivinhação/index.js
@@ -5,6 +5,8 @@ var chute = prompt(`Digite um numero de 1 a 1000`)
 
 if(chute == numSecreto) {
     alert("Parabens voce acertou!!")
+} if (chute < numSecreto) {
+    alert(`Você errou... o numero é maior que o numero digitado!`)
 } else {
-    alert("Anao... Voce errou...")
+    alert(`Você errou... O numero é menor que o numero digitado!`)
 }
