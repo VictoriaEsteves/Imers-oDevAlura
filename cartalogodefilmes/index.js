f955c41e49df21a90b5e5a99a65c0e423099da8a
Victoria Esteves <138332052+VictoriaEsteves@users.noreply.github.com>
2023-11-15 13:43:07 -0300

    Update index.js

--- cartalogodefilmes/index.js
+++ cartalogodefilmes/index.js
@@ -1,11 +1,6 @@
+var listaFilmes = ['https://cinepop.com.br/wp-content/uploads/2016/12/shadowhunters_ver16_xlg.jpg','https://br.web.img3.acsta.net/pictures/14/03/31/19/28/462555.jpg','https://upload.wikimedia.org/wikipedia/pt/4/40/Alice_in_Borderland_%28s%C3%A9rie%29.png', 'https://m.media-amazon.com/images/I/71HGgigujnL._AC_UF1000,1000_QL80_.jpg','https://br.web.img2.acsta.net/pictures/15/07/29/14/06/297599.jpg','https://s2.glbimg.com/Mzm9GgdZAG6CWhEobVY3ZpcmuEc=/e.glbimg.com/og/ed/f/original/2018/10/14/sabrina_vertical-main_rgb.jpg','https://i.pinimg.com/236x/01/4f/0d/014f0de7ec13834005924c8e27a7270e.jpg', 'https://br.web.img2.acsta.net/pictures/22/12/06/21/04/3971225.jpg']
 
-var listaFilmes = ['https://cinepop.com.br/wp-content/uploads/2016/12/shadowhunters_ver16_xlg.jpg','https://br.web.img3.acsta.net/pictures/14/03/31/19/28/462555.jpg','https://upload.wikimedia.org/wikipedia/pt/4/40/Alice_in_Borderland_%28s%C3%A9rie%29.png', 'https://m.media-amazon.com/images/I/71HGgigujnL._AC_UF1000,1000_QL80_.jpg','https://br.web.img2.acsta.net/pictures/15/07/29/14/06/297599.jpg']
-
-var nomesFilmes = ['Shadowhunter', 'Breaking Bad', 'Alice in Borderland', 'Suits', 'Narcos' ]
-/*listaFilmes[0] = 'https://static.wikia.nocookie.net/shadowhunterstv/images/b/ba/TMI2Promo_Poster01.jpg/revision/latest/scale-to-width-down/1200?cb=20180219132636&path-prefix=pt-br'
-listaFilmes[1] = 'https://br.web.img3.acsta.net/pictures/14/03/31/19/28/462555.jpg'
-listaFilmes[2] = 'https://upload.wikimedia.org/wikipedia/pt/4/40/Alice_in_Borderland_%28s%C3%A9rie%29.png'*/
-
+var nomesFilmes = ['Shadowhunter', 'Breaking Bad', 'Alice in Borderland', 'Suits', 'Narcos', 'O Mundo Sombrio de Sabrina', 'Supernatural', 'The Witcher' ]
 
 for(var i=0;i < listaFilmes.length; i++) {
 
