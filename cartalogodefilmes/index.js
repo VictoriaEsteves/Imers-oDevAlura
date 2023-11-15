e560e96df28644f07dff530a73af4d94d7f8092f
Victoria Esteves <138332052+VictoriaEsteves@users.noreply.github.com>
2023-11-15 11:11:03 -0300

    Update index.js

--- cartalogodefilmes/index.js
+++ cartalogodefilmes/index.js
@@ -1,5 +1,5 @@
 
-var listaFilmes = ['https://static.wikia.nocookie.net/shadowhunterstv/images/b/ba/TMI2Promo_Poster01.jpg/revision/latest/scale-to-width-down/1200?cb=20180219132636&path-prefix=pt-br','https://br.web.img3.acsta.net/pictures/14/03/31/19/28/462555.jpg','https://upload.wikimedia.org/wikipedia/pt/4/40/Alice_in_Borderland_%28s%C3%A9rie%29.png']
+var listaFilmes = ['https://cinepop.com.br/wp-content/uploads/2016/12/shadowhunters_ver16_xlg.jpg','https://br.web.img3.acsta.net/pictures/14/03/31/19/28/462555.jpg','https://upload.wikimedia.org/wikipedia/pt/4/40/Alice_in_Borderland_%28s%C3%A9rie%29.png']
 
 /*listaFilmes[0] = 'https://static.wikia.nocookie.net/shadowhunterstv/images/b/ba/TMI2Promo_Poster01.jpg/revision/latest/scale-to-width-down/1200?cb=20180219132636&path-prefix=pt-br'
 listaFilmes[1] = 'https://br.web.img3.acsta.net/pictures/14/03/31/19/28/462555.jpg'
