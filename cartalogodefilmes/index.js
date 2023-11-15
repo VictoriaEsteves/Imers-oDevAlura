9427b7257f00bc980d4208b35a908cb84428bd19
Victoria Esteves <138332052+VictoriaEsteves@users.noreply.github.com>
2023-11-15 11:09:58 -0300

    Update index.js

---
+++ cartalogodefilmes/index.js
@@ -0,0 +1,13 @@
+
+var listaFilmes = ['https://static.wikia.nocookie.net/shadowhunterstv/images/b/ba/TMI2Promo_Poster01.jpg/revision/latest/scale-to-width-down/1200?cb=20180219132636&path-prefix=pt-br','https://br.web.img3.acsta.net/pictures/14/03/31/19/28/462555.jpg','https://upload.wikimedia.org/wikipedia/pt/4/40/Alice_in_Borderland_%28s%C3%A9rie%29.png']
+
+/*listaFilmes[0] = 'https://static.wikia.nocookie.net/shadowhunterstv/images/b/ba/TMI2Promo_Poster01.jpg/revision/latest/scale-to-width-down/1200?cb=20180219132636&path-prefix=pt-br'
+listaFilmes[1] = 'https://br.web.img3.acsta.net/pictures/14/03/31/19/28/462555.jpg'
+listaFilmes[2] = 'https://upload.wikimedia.org/wikipedia/pt/4/40/Alice_in_Borderland_%28s%C3%A9rie%29.png'*/
+
+
+for(var i=0; i<3; i++) {
+
+document.write("<img src=" + listaFilmes[i] + ">")
+
+}
