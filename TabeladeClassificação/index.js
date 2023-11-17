3b003c066d93af975726dd564562028eff5fe337
Victoria Esteves <138332052+VictoriaEsteves@users.noreply.github.com>
2023-11-17 19:07:08 -0300

    Update Index.js and index.html

--- TabeladeClassificação/index.js
+++ TabeladeClassificação/index.js
@@ -1,6 +1,35 @@
+var victoria = {
+    nome: 'Victoria',
+    vitoria: 0,
+    empate: 0,
+    derrota: 0,
+    pontos: 0
+}
 
-function adicionarVitoria() {
+var lucas = {
+    nome: 'Lucas',
+    vitoria: 0,
+    empate: 0,
+    derrota: 0,
+    pontos: 0
+}
 
+var elementoTabela = document.getElementById('tabelaJogadores')
+elementoTabela.innerHTML = `
+<tr>
+<td>${victoria.nome}</td>
+<td>${victoria.vitoria}</td>
+<td>${victoria.empate}</td>
+<td>${victoria.derrota}</td>
+<td>${victoria.pontos}</td>
+<td><button onClick="adicionarVitoria()">Vitória</button></td>
+<td><button onClick="adicionarEmpate()">Empate</button></td>
+<td><button onClick="adicionarDerrota()">Derrota</button></td>
+</tr>
+`
+
+function adicionarVitoria(jogador) {
+console.log(victoria)
 }
 
 function adicionarEmpate() {
@@ -8,5 +37,5 @@ function adicionarEmpate() {
 }
 
 function adicionarDerrota() {
-    
+
 }
