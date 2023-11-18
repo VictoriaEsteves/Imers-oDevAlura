bf52d1646f8eeac6694aef9f8e9476125c5763f1
Victoria Esteves <138332052+VictoriaEsteves@users.noreply.github.com>
2023-11-18 13:26:57 -0300

    Challenges - Tabela de Classificação

---
+++ TabeladeClassificação/desafios(3)/GuiLima/index.js
@@ -0,0 +1,25 @@
+var arrayFilmes = []
+
+function adicionarFilme() {
+    var valorFilme = document.getElementById('filme').value
+
+    if(valorFilme.endsWith('jpg') || valorFilme.endsWith('JPG')) {
+        arrayFilmes.push(valorFilme)
+        document.getElementById('filme').value = ''
+
+        carregarFilmes()
+    }
+
+}
+
+function carregarFilmes() {
+    var espacoFilmes = document.getElementById('listaFilmes')
+    espacoFilmes.innerHTML = '';
+    
+    for(var i = 0; i < arrayFilmes.length; i++) {
+        var filme = arrayFilmes[i]
+        var imgFilme = document.createElement('img');
+        imgFilme.src = filme;
+        espacoFilmes.appendChild(imgFilme);
+    }
+}
