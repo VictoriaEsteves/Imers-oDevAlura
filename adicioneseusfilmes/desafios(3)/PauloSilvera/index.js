e6d98b102e5b10c4ef3a9945ee04c955f23ae2bc
Victoria Esteves <138332052+VictoriaEsteves@users.noreply.github.com>
2023-11-18 12:40:16 -0300

    Update index.js

---
+++ adicioneseusfilmes/desafios(3)/PauloSilvera/index.js
@@ -0,0 +1,8 @@
+var filmeFavorito = []
+var trailerFavorito = []
+
+function adicionarFilme() {
+    var valorFilme = document.getElementById('filme').value
+    var valorTrailer = document.getElementById('trailer').value
+    
+}
