6ac0a59dc941cec286aaa5622c8a35e8122ffb86
Victoria Esteves <138332052+VictoriaEsteves@users.noreply.github.com>
2024-01-26 13:01:19 -0300

    some changes

--- front-end/imerçãoDev/script.js
+++ front-end/imerçãoDev/script.js
@@ -1,4 +1,3 @@
-
 const searchInput = document.getElementById('search-input');
 const resultArtist = document.getElementById("result-artist");
 const resultPlaylist = document.getElementById('result-playlists');
