5208f9dc1b245ed2ce852bbab8da6073669e6162
Victoria Esteves <138332052+VictoriaEsteves@users.noreply.github.com>
2024-01-26 15:04:13 -0300

    Update script.js

--- front-end/imersãoDev/script.js
+++ front-end/imersãoDev/script.js
@@ -3,16 +3,16 @@ const resultArtist = document.getElementById("result-artist");
 const resultPlaylist = document.getElementById('result-playlists');
 
 function requestApi(searchTerm) {
-    const url = "http://localhost:3000/artists?name_like" + searchTerm
+    const url = "http://localhost:3000/artists?name_like=" + searchTerm
     fetch(url)
         .then((response) => response.json())
-        .then((result) => displayResults(result))
+        .then((result) => displayResults(result));
 }
 
 function displayResults(result) {
     resultPlaylist.classList.add("hidden")
-    const artistName = document.getElementById('artist-name');
-    const artistImage = document.getElementById('artist-img');
+    const artistName = document.getElementById("artist-name");
+    const artistImage = document.getElementById("artist-img");
 
     result.forEach(element => {
         artistName.innerText = element.name;
